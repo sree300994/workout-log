4f8429c24f3a87648107530e975aedd6231ba17e
sree300994 <sk300994@gmail.com>
2017-09-24 20:56:44 +0530

    Define Actions and View Files for Controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,43 @@
 class WorkoutsController < ApplicationController
+
+	before_action :set_workout, only: [:show, :edit, :update, :destroy]
+	def index
+		@workouts = Workout.all
+	end
+
+	def new
+		@workout = Workout.new
+	end
+
+	def create
+		@workout = Workout.new(workout_params)
+		if @workout.save
+			redirect_to workouts_path, notice: "Successfully created the Workout"
+		else
+			render action: "new"
+		end
+	end
+
+	def edit
+		
+	end
+
+	def show
+		
+	end
+
+	def destroy
+		@workout.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def set_workout
+		@workout = Workout.find(params[:id]) 
+	end
+
+	def workout_params
+		params.require(:workout).permit(:date, :workout, :mood, :length)
+	end
 end
